ee637bc3d16f33ac21bd0598aaa8be409a817a46
Laszlo Bacsi <lackac@lackac.hu>
2009-02-18 04:10:04 +0100

    Added models

---
+++ model.rb
@@ -0,0 +1,32 @@
+require 'dm-core'
+require 'dm-timestamps'
+
+DataMapper.setup(:default, :adapter => :sqlite3, :database => "db/#{Sinatra::Application.environment}.sqlite3")
+
+class Profile
+  include DataMapper::Resource
+
+  property :id,           String, :key => true
+  property :display_name, String
+  property :profile_url,  String
+  property :created_at,   Time
+
+  has n, :interests
+  has n, :marked_profiles, :through => :interests, :remote_name => :profile,
+         :class_name => "Profile", :child_key => [:profile_id]
+  has n, :marked_by, :through => :interests, :remote_name => :interested_in,
+         :class_name => "Profile", :child_key => [:interested_in_id]
+end
+
+class Interest
+  include DataMapper::Resource
+
+  property :profile_id,       String, :key => true
+  property :interested_in_id, String, :key => true
+  property :created_at,       Time
+
+  belongs_to :profile, :child_key => [:profile_id]
+  belongs_to :interested_in, :child_key => [:interested_in_id], :class_name => "Profile"
+end
+
+DataMapper.auto_upgrade!
